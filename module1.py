32e1f84fe5fd5fd8d90cc08cb61abc02bc510d30
Zufarro <zufar-galimzyanov@yandex.ru>
2019-03-16 21:15:51 +0300

    Bugs fixed

--- module1.py
+++ module1.py
@@ -7,58 +7,58 @@ class CarRentService():
         self.car_hisory = {}
         self.earnings_by_clients = {}
 
-    def add_car(self,model,price):
-        self.storage_all[model] = price
-        self.free_cars[model] = price
+    def add_car(self,RentCar):
+        self.storage_all[RentCar.model] = RentCar.rent_price
+        self.free_cars[RentCar.model] = RentCar.rent_price
 
-    def rent_car(self, rent_model, client_name):
-        if rent_model in self.free_cars:
-            del self.free_cars[rent_model]
-            if client_name in self.client_base:
-                self.client_base[client_name].append(rent_model)
+    def rent_car(self, RentCar, Client):
+        if RentCar.model in self.free_cars:
+            del self.free_cars[RentCar.model]
+            if Client.name in self.client_base:
+                self.client_base[Client.name].append(RentCar.model)
             else:
-                self.client_base.update({client_name:[rent_model]})
-        elif rent_model in self.storage_all and rent_model not in self.free_cars:
-            print ("Модель", rent_model, "арендуется")
+                self.client_base.update({Client.name:[RentCar.model]})
+        elif RentCar.model in self.storage_all and RentCar.model not in self.free_cars:
+            print ("Модель", RentCar.model, "арендуется")
         else:
-            print ("Модель", rent_model, "не найдена")
+            print ("Модель", RentCar.model, "не найдена")
 
     def get_total_earnings(self):
         return "Выручка сервиса: " + str(sum(self.cash_base)) + " $"
 
-    def get_client_history(self,find_client):
-        if find_client in self.client_base:
-            print(find_client, "у нас арендовывал :",self.client_base[find_client])
+    def get_client_history(self,Client):
+        if Client.name in self.client_base:
+            print(Client.name, "у нас арендовывал :",self.client_base[Client.name])
         else:
-            print("С таким именем не было клиента")
+            print("Клиент "+str(Client.name)+" пока у нас не арендовывал авто")
 
-    def return_car(self, return_model, client_name, hours):
+    def return_car(self, RentCar, Client, hours):
         if hours > 0:
-            if return_model in self.storage_all and return_model not in self.free_cars:
-                cash = int(self.storage_all.get(return_model))*int(hours)
-                self.free_cars.update({return_model:self.storage_all.get(return_model)})
+            if RentCar.model in self.storage_all and RentCar.model not in self.free_cars:
+                cash = int(self.storage_all.get(RentCar.model))*int(hours)
+                self.free_cars.update({RentCar.model:self.storage_all.get(RentCar.model)})
                 self.cash_base.append(cash)
-                if return_model in self.car_hisory:
-                    self.car_hisory[return_model].append(cash)
+                if RentCar.model in self.car_hisory:
+                    self.car_hisory[RentCar.model].append(cash)
                 else:
-                    self.car_hisory.update({return_model:[cash]})
-                if  client_name in self.earnings_by_clients:
-                    self.earnings_by_clients[client_name].append(cash)
+                    self.car_hisory.update({RentCar.model:[cash]})
+                if  Client.name in self.earnings_by_clients:
+                    self.earnings_by_clients[Client.name].append(cash)
                 else:
-                    self.earnings_by_clients.update({client_name:[cash]})
+                    self.earnings_by_clients.update({Client.name:[cash]})
             else:
-                print(return_model, " - Вы возвращаете не ту машину, которую арендовали!")
+                print(RentCar.model, " - Вы возвращаете не ту машину, которую арендовали!")
 
-    def get_rent_history(self,model):
-        if model in self.car_hisory:
-            print("Автомобиль арендовался:", len(self.car_hisory[model]), "раз")
-            print("Автомобиль принес доход:", sum(self.car_hisory[model]), "$")
+    def get_rent_history(self,RentCar):
+        if RentCar.model in self.car_hisory:
+            print("Автомобиль арендовался:", len(self.car_hisory[RentCar.model]), "раз")
+            print("Автомобиль принес доход:", sum(self.car_hisory[RentCar.model]), "$")
         else:
-            print("Автомобиль еще не арендовался или отсутсвует в сервисе")
+            print("Автомобиль еще не арендовался")
 
-    def get_earnings_by_client(self,find_name):
-        if find_name in self.earnings_by_clients:
-            print("Клиент принес доход:", sum(self.earnings_by_clients[find_name]), "$")
+    def get_earnings_by_client(self,Client):
+        if Client.name in self.earnings_by_clients:
+            print("Клиент принес доход:", sum(self.earnings_by_clients[Client.name]), "$")
         else:
             print("Клиент не найден")
 
--- module2.py
+++ module2.py
@@ -4,35 +4,39 @@ my_rent_service = CarRentService()
 
 car1 = RentCar("kia", 50)
 car2 = RentCar("bmw", 150)
-my_rent_service.add_car(car1.model, car1.rent_price)
-my_rent_service.add_car(car2.model, car2.rent_price)
+my_rent_service.add_car(car1)
+my_rent_service.add_car(car2)
 
 print("Все машины сервиса: ", my_rent_service.storage_all)
 
+
 client1 = Client("Bob")
-my_rent_service.rent_car(car1.model, client1.name)
+my_rent_service.rent_car(car1, client1)
 
 print("Своблодные машины сервиса: ", my_rent_service.free_cars)
 print("История заказов: ", my_rent_service.client_base)
 
-my_rent_service.return_car(car1.model, client1.name, 10)
+
+my_rent_service.return_car(car1, client1, 10)
 
 print("Своблодные машины сервиса: ", my_rent_service.free_cars)
+
 my_rent_service.get_total_earnings()
 
-my_rent_service.get_rent_history(car1.model)
+my_rent_service.get_rent_history(car1)
 
-my_rent_service.get_client_history(client1.name)
 
-my_rent_service.get_earnings_by_client(client1.name)
+my_rent_service.get_client_history(client1)
+
+my_rent_service.get_earnings_by_client(client1)
 
 client2 = Client("Alex")
-my_rent_service.rent_car(car2.model, client2.name)
+my_rent_service.rent_car(car2, client2)
 
-my_rent_service.return_car(car2.model, client1.name, 120)
+my_rent_service.return_car(car2, client1, 120)
 
 print("История заказов: ", my_rent_service.client_base)
-my_rent_service.get_rent_history(car2.model)
+my_rent_service.get_rent_history(car2)
 
 f = open("base_car.txt", "w")
 f.write(",".join(my_rent_service.free_cars))
